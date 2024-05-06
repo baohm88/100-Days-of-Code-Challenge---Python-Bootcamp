f7d63d719a632cf47df0a44f199a814237196ed4
Bao Ha <baohm88@gmail.com>
2024-05-06 09:30:09 +0700

    Added data types, number manipulation and f-string format in py

---
+++ day002/number_manipulation.py
@@ -0,0 +1,12 @@
+print(round( 8 / 3, 2))
+print(8 // 3)
+result = 4 / 2
+result /= 2
+print(result)
+
+# F string
+score = 0
+height = 1.8
+isWinning = True
+
+print(f"Your score is {score}, your height is {height}, you're winning is {isWinning}")
---
+++ day002/your_life_in_weeks.py
@@ -0,0 +1,6 @@
+age = input("What's your age? ")
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+remaining_years = 90 - int(age)
+remaining_weeks = remaining_years * 52
+print(f"You have {remaining_weeks} weeks left.")
