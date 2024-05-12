cede58fdd0692caf9edc53e33d25c0448e5e1615
Bao Ha <baohm88@gmail.com>
2024-05-12 10:06:52 +0700

    Added paint calc

--- day008/greet.py
+++ day008/greet.py
@@ -1,16 +1,24 @@
 # Def fn without parameter
 
-def greet():
-  print('Hello')
-  print('How do you do?')
-  print("Isn't the weather nice today?")
+# def greet():
+#   print('Hello')
+#   print('How do you do?')
+#   print("Isn't the weather nice today?")
 
-greet()
+# greet()
 
 # FN with 1 para
-def greet_with_name(name):
-  print(f'Hello, {name}')
-  print('How do you do ' + name + "?")
-  print("Isn't the weather nice today?")
+# def greet_with_name(name):
+#   print(f'Hello, {name}')
+#   print('How do you do ' + name + "?")
+#   print("Isn't the weather nice today?")
 
-greet_with_name('Bao Ha')
+# greet_with_name('Bao Ha')
+
+def greet_with(name, location):
+  print(f"Hello, {name}!")
+  print(f"What's it like in {location}?")
+
+greet_with(name='Bao',location= 'Hanoi')
+greet_with(location= 'Hanoi', name='Bao')
+
