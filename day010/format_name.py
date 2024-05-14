483039ded108d94934592343fa69cc5eae3a15b1
Bao Ha <baohm88@gmail.com>
2024-05-14 08:50:45 +0700

    Added empty name checker

--- day010/format_name.py
+++ day010/format_name.py
@@ -1,4 +1,7 @@
 def format_name(f_name, l_name):
+    if f_name == '' or l_name == '':
+        return "You didn't provide valid inputs."
+    
     return f"{f_name.title()} {l_name.title()}" 
 
 # f_name = input("First name: ")
